5a6b1b23bc4dbc4949090b937b2ea4aeffc4d87b
szglabs <sdrummond95@gmail.com>
2021-01-14 00:39:29 -0500

    full UI modal/form built out

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let myLibrary = [
     }
 ];
 
-function Book(title, author, pages, notes, isRead) {
+function Book(title, author, pages, isRead, notes) {
     this.title = title
     this.author = author
     this.pages = pages
@@ -29,30 +29,58 @@ function Book(title, author, pages, notes, isRead) {
 };
 
 function addBook () {
-    let newBook = new Book(title, author, pages, notes, isRead)
+    let newBook = new Book(title, author, pages, isRead, notes)
     myLibrary.push(newBook);
 }
 
 function displayBooks () {
-    myLibrary.forEach(function (book) {
+
+    document.querySelector('.books').innerHTML = '';
+    myLibrary.forEach(function (book, i) {
         let div = document.createElement('div');
         div.setAttribute('class', 'col l4 m6 s12');
-        div.innerHTML = 
-            `<div class="card small z-depth-3">
-            <div class="card-content">
-                <span class="card-title">${book.title}</span>
-                <p class="author">By: ${book.author}</p>
-                <p class="pages">Number of Pages: ${book.pages}</p>
-                <p class="isRead">Completed: ${book.isRead}</p>
-            </div>
-            <div class="card-action">
-                <a class="btn-flat">Notes</a>
-                <a class="btn-flat">Edit</a>
-                <a class="btn-flat">Delete</a>
-            </div>
-        </div>`
+        div.innerHTML = `
+            <div class="card small z-depth-3">
+                <div class="card-content">
+                    <span class="card-title">${book.title}</span>
+                    <p class="author">By: ${book.author}</p>
+                    <p class="pages">Number of Pages: ${book.pages}</p>
+                    <p class="isRead">Completed: ${book.isRead}</p>
+                </div>
+                <div class="card-action">
+                    <a class="btn-flat">Edit</a>
+                    <a class="btn-flat delete">Delete</a>
+                </div>
+            </div>`
         document.querySelector('.books').appendChild(div);
     })
 }
 
 displayBooks();
+
+const deleteBtn = document.querySelectorAll('.delete')
+deleteBtn.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        
+    })
+})
+
+// Modal form
+document.addEventListener('DOMContentLoaded', function () {
+    var elem = document.querySelector('.modal');
+    const options = {
+        dismissible: false
+    }
+    var instance = M.Modal.init(elem, options)
+    
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
